2e66cf0313f6835bc551d07c9ea3f2c82ba3ea2f
Andymonzon <valentinomonzonandres@gmail.com>
2024-07-29 19:54:14 -0300

    feat: Implement project creation

--- src/components/ContainerCards/Card/Menu/Menu.tsx
+++ src/components/ContainerCards/Card/Menu/Menu.tsx
@@ -3,14 +3,21 @@ import EditIcon from "@/../public/editIcon.svg";
 import DeleteIcon from "@/../public/deleteIcon.svg";
 import { useRef } from "react";
 import useOnClickOutside from "@/hooks/useOnClickOutside";
+import { deleteProject } from "@/store/project/slice";
+import { useAppDispatch } from "@/hooks/store";
 
 interface Props {
   openModal: boolean;
   setOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
+  id: string;
 }
 
-export const Menu = ({ openModal, setOpenModal }: Props) => {
+export const Menu = ({ openModal, setOpenModal, id }: Props) => {
   const modalRef = useRef(null);
+  const dispatch = useAppDispatch();
+  const handleDelete = () => {
+    dispatch(deleteProject(id));
+  };
 
   useOnClickOutside(modalRef, () => setOpenModal(false));
   return (
@@ -29,7 +36,7 @@ export const Menu = ({ openModal, setOpenModal }: Props) => {
               Edit
             </button>
             <button
-              onClick={(e) => console.log(e)}
+              onClick={handleDelete}
               className="flex p-1 bg-white gap-2 items-center"
             >
               <Image src={DeleteIcon} alt="delete icon" />
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,9 +1,48 @@
+"use client";
+
+import { useAppDispatch } from "@/hooks/store";
+import { addProject, Project } from "@/store/project/slice";
+
 export const Form = () => {
+  const dispatch = useAppDispatch();
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = Object.fromEntries(
+      new FormData(e.target as HTMLFormElement)
+    );
+
+    const { description, projectName, asiggnedTo, projectManager, status } =
+      formData as {
+        description: string;
+        projectName: string;
+        asiggnedTo: string;
+        projectManager: string;
+        status: string;
+      };
+
+    const newProject: Project = {
+      id: crypto.randomUUID(),
+      createdAt: new Date(),
+      name: "Andy",
+      description,
+      projectName,
+      asiggnedTo,
+      projectManager,
+      status,
+    };
+    dispatch(addProject(newProject));
+  };
+
   return (
-    <form className="px-5 py-2 flex flex-col gap-4 mt-2">
+    <form
+      onSubmit={handleSubmit}
+      className="px-5 py-2 flex flex-col gap-4 mt-2"
+    >
       <div className="flex flex-col">
         <label>Project Name</label>
         <input
+          name="projectName"
           type="text"
           className="border-2 border-[#BFBFBF] rounded outline-none px-1 py-2"
         />
@@ -11,26 +50,36 @@ export const Form = () => {
       <div className="flex flex-col">
         <label>Description</label>
         <input
+          name="description"
           type="text"
           className="border-2 border-[#BFBFBF] rounded outline-none px-1 py-2"
         />
       </div>
       <div className="flex flex-col">
         <label>Project manager</label>
-        <select className="border-2 border-[#BFBFBF] rounded outline-none px-1 py-2">
-          <option defaultValue="">Select a person</option>
+        <select
+          name="projectManager"
+          className="border-2 border-[#BFBFBF] rounded outline-none px-1 py-2"
+        >
+          <option value="">Select a person</option>
         </select>
       </div>
       <div className="flex flex-col">
         <label>Asiggned to</label>
-        <select className="border-2 border-[#BFBFBF] rounded outline-none px-1 py-2">
-          <option defaultValue="">Select a person</option>
+        <select
+          name="asiggnedTo"
+          className="border-2 border-[#BFBFBF] rounded outline-none px-1 py-2"
+        >
+          <option value="">Select a person</option>
         </select>
       </div>
       <div className="flex flex-col">
         <label>Status</label>
-        <select className="border-2 border-[#BFBFBF] rounded outline-none px-1 py-2">
-          <option defaultValue="">Enabled</option>
+        <select
+          name="status"
+          className="border-2 border-[#BFBFBF] rounded outline-none px-1 py-2"
+        >
+          <option value="">Enabled</option>
         </select>
       </div>
       <button className="bg-[#F5222D] text-white p-2 rounded w-max">
