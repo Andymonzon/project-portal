9a46d44a0d96ccc301c4a690d44eb0d8412eea41
Andymonzon <valentinomonzonandres@gmail.com>
2024-07-30 14:15:26 -0300

    feat: add Suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import { Navbar } from "@/components/Navbar/Navbar";
 import { ContainerCards } from "../components/ContainerCards/ContainerCards";
 import { types } from "@/models/navbar.model";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <>
       <Navbar type={types.HOME} />
       <main className="flex flex-col items-center justify-center h-full">
-        <ContainerCards />
+        <Suspense fallback={<div>Loading...</div>}>
+          <ContainerCards />
+        </Suspense>
       </main>
     </>
   );
